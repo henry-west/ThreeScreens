e529234bb7c4e29d4b74d64adc46af39b34629c3
Henry West <henry@Henrys-MBA.local>
2014-09-10 16:33:53 -0500

    Added additional screens and transitions
    Added new screens for Meredith and Giuliani to edit

--- src/edu/drake/threescreens/GiulianiScreen.java
+++ src/edu/drake/threescreens/GiulianiScreen.java
@@ -1,34 +1,22 @@
 package edu.drake.threescreens;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class GiulianiScreen extends Activity {
 
-	public void screenTwo(View view) {
-		Intent intent = new Intent(this, MeredithScreen.class);
-		startActivity(intent);
-	}
-	
-	public void screenThree(View view) {
-		Intent intent = new Intent(this, GiulianiScreen.class);
-		startActivity(intent);
-	}
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_giuliani_screen);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.giuliani_screen, menu);
 		return true;
 	}
 
--- src/edu/drake/threescreens/MeredithScreen.java
+++ src/edu/drake/threescreens/MeredithScreen.java
@@ -1,34 +1,22 @@
 package edu.drake.threescreens;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class MeredithScreen extends Activity {
 
-	public void screenTwo(View view) {
-		Intent intent = new Intent(this, MeredithScreen.class);
-		startActivity(intent);
-	}
-	
-	public void screenThree(View view) {
-		Intent intent = new Intent(this, GiulianiScreen.class);
-		startActivity(intent);
-	}
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_meredith_screen);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.meredith_screen, menu);
 		return true;
 	}
 
